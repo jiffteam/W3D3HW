2f9d13768060fc61dbd2080ebd59ba2cc71ba9c7
kameron <kamscott3@gmail.com>
2022-04-13 16:33:15 -0400

    jump

---
+++ app.js
@@ -0,0 +1,17 @@
+var a = 10;
+var b = 20;
+var c = 30;
+var b = 40;
+
+console.log(a+b);
+console.log(c+d);
+
+console.log(a-c);
+console.log(c-d);
+
+console.log(b*c);
+console.log(c*d);
+
+console.log(a/d);
+console.log(c/d);
+
